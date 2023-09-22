bdc30ab3791e6c883f6f8bad7823415d11e9cc9e
anna-roses <anna.roses.marti@gmail.com>
2023-09-22 12:14:29 +0200

    creation of two objects of animals and content of gitignore

---
+++ index.js
@@ -0,0 +1,17 @@
+const animal1 = {
+  id: 1,
+  name: "delfi",
+  ageYears: 2,
+  weightInKg: 170,
+  species: "Delfín Baiji",
+  extinctionYear: 2007,
+};
+
+const animal2 = {
+  id: 2,
+  name: "scrat",
+  ageYears: 3,
+  weightInKg: 4,
+  species: "ardilla dientes de sable",
+  extinctionYear: -60000000,
+};
